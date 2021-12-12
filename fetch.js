32141db82cf0b7c4029d2d3cb3a67483dc8facff
Vaibhav Singh <61027919+vaibhav1997756@users.noreply.github.com>
2021-12-12 18:41:38 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,15 +1,10 @@
 let r = fetch('https://reqres.in/api/user');
-
 let s = r.then(function(res) {
     return res.json();
 });
-
 s.then(function(a) {
-    // console.log(data);
     displayData(a.data);
 });
-
-
 function displayData(data) {
     data.forEach(function(element) {
         console.log(element);
